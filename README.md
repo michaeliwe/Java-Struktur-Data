ef3f1613e948b94392ddfe80f29f2b07adaec03f
Michaeliwe <michael.liwe11@gmail.com>
2018-09-08 01:08:39 +0700

    test

--- README.md
+++
@@ -1,2 +0,0 @@
-# STRUKDAT
-Ini merupakan github tentang apa saja yang saya pelajari di kuliah semester tiga dengan matkul bernama STRUKDAT
---
+++ src/doubleLinkedList/DoublyDoubleLinkListedApp.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package doubleLinkedList;
+
+/**
+ *
+ * @author mike
+ */
+////////////////////////////////////////////////////////////////
+class DoublyDoubleLinkListedApp
+   {
+   public static void main(String[] args)
+      {                             // make a new list
+      DoublyDoubleLinkListedList theList = new DoublyDoubleLinkListedList();
+
+      theList.insertFirst(22);      // insert at front
+      theList.insertFirst(44);
+      theList.insertFirst(66);
+
+      theList.insertLast(11);       // insert at rear
+      theList.insertLast(33);
+      theList.insertLast(55);
+
+      theList.displayForward();     // display list forward
+      theList.displayBackward();    // display list backward
+
+      theList.deleteFirst();        // delete first item
+      theList.deleteLast();         // delete last item
+      theList.deleteKey(11);        // delete item with key 11
+
+      theList.displayForward();     // display list forward
+
+      theList.insertAfter(22, 77);  // insert 77 after 22
+      theList.insertAfter(33, 88);  // insert 88 after 33
+
+      theList.displayForward();     // display list forward
+      }  // end main()
+   }  // end class DoublyDoubleLinkListedApp
+////////////////////////////////////////////////////////////////
